10567ca6a359dfdaa6910e641ec449b608bb8d77
Conor-Harney <cw208kccgdbc00155687@gmail.com>
2018-02-20 11:54:52 +0000

    added a comment

--- src/main/java/com/qa/application/App.java
+++ src/main/java/com/qa/application/App.java
@@ -7,6 +7,8 @@ import com.qa.util.JSONUtil;
 public class App {
 
 	public static void main(String[] args) {
+		
+		//added a comment
 		AccountService service = new AccountService();
 		JSONUtil util = new JSONUtil();
 		Account joeBloggs = new Account("Joe", "Bloggs", "1234");
