ab6016480fcb87ec344c4a8d929fbd4adad7ba3b
NiallMul <nmulready@hotmail.com>
2018-02-21 15:03:27 +0000

    Sql file

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into Account(firstName, secondName, accountNumber) values('John', 'Bloogs', 'JM001');
+insert into Account(firstName, secondName, accountNumber) values('Jane', 'Bloogs', 'JM002');
+insert into Account(firstName, secondName, accountNumber) values('John', 'Gordon', 'JG003');
